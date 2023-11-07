6787324685e4be8c40198b25ab9b4142f5aa37b7
Andreas Dantha Sianturi <andrdantha@gmail.com>
2023-11-07 19:29:21 +0700

    forgot to commit prior changes

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 from datetime import datetime
 import requests
 from bson import ObjectId
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("mongodb+srv://test:sparta@cluster0.gz7pp7h.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp")
+DB_NAME =  os.environ.get("plus_week2")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
 
 app = Flask(__name__)
-
-client = MongoClient('mongodb+srv://test:sparta@cluster0.gz7pp7h.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp')
-db = client.dbsparta_plus_week2
-
 
 @app.route('/')
 def main():
